3a2cdf48eda76d428cdde4179a4bc4da48982354
Yago Moreira <yagomoreira@gmail.com>
2024-07-09 09:27:04 -0300

    feat: add confirm participant

---
+++ backend/src/routes/confirm-participant.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { prisma } from "../lib/prisma";
+
+
+export async function confirmParticipants(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/participants/:participantId/confirm",
+    {
+      schema: {
+        params: z.object({
+          participantId: z.string().uuid()
+        })
+      },
+    }, async (request, reply) => {
+      const { participantId } = request.params;
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          id: participantId
+        }
+      })
+
+      if (!participant) {
+        throw new Error('Participant not found.')
+      }
+
+      if (participant.is_confirmed) {
+        return reply.redirect(`http://localhost:3000/trips/${participant.email}`)
+      }
+
+      await prisma.participant.update({
+        where: { id: participantId },
+        data: { is_confirmed: true }
+      })
+
+      return reply.redirect(`http://localhost:3000/trips/${participant.email}`)
+    },
+  )
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { createTrip } from './routes/create-trip';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { confirmTrip } from './routes/confirm-trip';
 import cors from "@fastify/cors"
+import { confirmParticipants } from './routes/confirm-participant';
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipants)
 
 app.listen({port: 3333}).then(() => {
   console.log("Server running");
