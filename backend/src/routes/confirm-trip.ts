a63402d12b400f692df81fc82e2e30f4d6bdffc2
Yago Moreira <yagomoreira@gmail.com>
2024-07-08 21:57:27 -0300

    refactor: create trip and add confirm trip

---
+++ backend/src/routes/confirm-trip.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+
+
+export async function confirmTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/confirm",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid()
+        })
+      },
+    }, async (request) => {
+      return { tripId: request.params.tripId }
+    });
+}
