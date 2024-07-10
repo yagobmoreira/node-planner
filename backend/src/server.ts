16038dcf81abf878247172406388b9db7af2722e
Yago Moreira <yagomoreira@gmail.com>
2024-07-10 08:49:04 -0300

    feat: add update trip, get participants and participant routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,15 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { confirmParticipants } from './routes/confirm-participant';
 import { confirmTrip } from './routes/confirm-trip';
 import { createActivity } from './routes/create-activity';
+import { createInvite } from "./routes/create-invite";
 import { createLink } from './routes/create-link';
 import { createTrip } from './routes/create-trip';
 import { getActivities } from './routes/get-activities';
 import { getLinks } from './routes/get-links';
+import { getParticipant } from "./routes/get-participant";
 import { getParticipants } from './routes/get-participants';
-import { createInvite } from "./routes/create-invite";
+import { getTripDetails } from "./routes/get-trip-details";
+import { updateTrip } from "./routes/update-trip";
 
 const app = fastify();
 
@@ -29,6 +32,9 @@ app.register(createLink)
 app.register(getLinks)
 app.register(getParticipants)
 app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({port: 3333}).then(() => {
   console.log("Server running");
